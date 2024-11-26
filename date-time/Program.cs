168197cf822ca4ab73ed04bf75db5124075aa3df
bogale0 <138402220+bogale0@users.noreply.github.com>
2024-11-26 21:56:44 +0900

    first

---
+++ date-time/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine($"DateOnly.Parse(): {DateOnly.Parse("2024-11-26")}");
+Console.WriteLine($"TimeOnly.ParseExact(): {TimeOnly.ParseExact("16.00", "HH.mm")}");
+TimeOnly time;
+if (TimeOnly.TryParse("8:30", out time))
+    Console.WriteLine($"TimeOnly.TryParse(): {time}");
+DateOnly date;
+if (DateOnly.TryParseExact("26-11-2024", "dd-MM-yyyy", out date))
+    Console.WriteLine($"DateOnly.TryParseExact(): {date}");
